44738c2e337b8c7fec287b14f3c767bf6c303395
骆金参 <luo960412@gmail.com>
2022-04-12 04:11:47 +0000

    adjust

--- mpa.config.js
+++ mpa.config.js
@@ -1,6 +1,8 @@
 exports.CONFIG = {
   //   entry: './_demo/**/*.{js,tsx}',
-  entry: './__demo/*.{tsx,jsx,js}',
+  // entry: './__demo/*.{tsx,jsx,js}',
 
   // entry: './__demo/page*.{tsx,jsx,js}',
+
+  entry: './_styled/*.{tsx,jsx,js}',
 };
